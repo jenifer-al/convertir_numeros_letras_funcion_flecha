aef4433aaad424b9388dcc9ceef4c527b3f6fd3c
jenifer-al <jenniferprat79@gmail.com>
2026-05-24 20:52:22 +0200

    Add files via upload

---
+++ js/letras.js
@@ -0,0 +1,35 @@
+
+
+const letras = [
+ "cero",
+ "uno",
+ "dos",
+ "tres",
+ "cuatro",
+ "cinco",
+ "seis",
+ "siete",
+ "ocho",
+ "nueve",
+
+];
+
+
+
+
+
+const validar = () => {
+
+    let input = parseInt(document.getElementById("numero").value);
+    let cajaResultado = document.getElementById("resultado");
+
+    if (isNaN(input) || input < 0 || input > 10) {
+        cajaResultado.innerText = "Número no válido";
+    } else {
+        // Usamos directamente 'numero' que ya lo tenemos arriba
+        cajaResultado.innerText = letras[input];
+    }
+
+
+
+}
